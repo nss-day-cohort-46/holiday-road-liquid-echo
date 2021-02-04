c5993578e2dcbeaf1f0e1a419b69d314dd223967
Nick Carver <nicholascarver2@outlook.com>
2021-02-04 10:17:23 -0600

    ready to test after merge

--- scripts/preview/PreviewList.js
+++ scripts/preview/PreviewList.js
@@ -1,11 +1,10 @@
 const targetElement = document.querySelector(".preview")
 eventHub = document.querySelector
-let previewHTML =""
-let
-let
-let
+let previewHTML = ""
+let parkHTML = ""
+let attractionHTML = ""
+let eateryHTML = ""
 
-<button class="disabled" id="saveItinerary">Save Itinerary</button>
 
 eventHub.addEventListener("parkSelected", event => {
     if (event.detail.id !==0) {
@@ -35,41 +34,34 @@ const render = () => {
     ${forecastHTML}
     ${eateryHTML}
     ${attractionHTML}
-    <article class="preview__forecast">
-    <div class="weatherCard">
-        <p>day1</p>
-        <p>temp</p>
-        <p>forecast</p>
-    </div>
-    <div class="weatherCard">
-        <p>day2</p>
-        <p>temp</p>
-        <p>forecast</p>
-    </div>
-    <div class="weatherCard">
-        <p>day3</p>
-        <p>temp</p>
-        <p>forecast</p>
-    </div>
-    <div class="weatherCard">
-        <p>day4</p>
-        <p>temp</p>
-        <p>forecast</p>
-    </div>
-    <div class="weatherCard">
-        <p>day5</p>
-        <p>temp</p>
-        <p>forecast</p>
-    </div>
-    </article>
-    <article class="preview__eatery">
-    <h3>Eatery Name</h3>
-    <button class ="detailsButton" id="showEateryDetails" value="eateryId">details</button>
-    </article>
-    <article class="preview__attraction">
-    <h3>Attraction Name</h3>
-    <button class="detailsButton" id="showAttractionDetails" value="attractionId">details</button>
-    </article>
+    <button class="disabled" id="saveItinerary">Save Itinerary</button>
     `
 }
 
+    // <article class="preview__forecast">
+    // <div class="weatherCard">
+    //     <p>day1</p>
+    //     <p>temp</p>
+    //     <p>forecast</p>
+    // </div>
+    // <div class="weatherCard">
+    //     <p>day2</p>
+    //     <p>temp</p>
+    //     <p>forecast</p>
+    // </div>
+    // <div class="weatherCard">
+    //     <p>day3</p>
+    //     <p>temp</p>
+    //     <p>forecast</p>
+    // </div>
+    // <div class="weatherCard">
+    //     <p>day4</p>
+    //     <p>temp</p>
+    //     <p>forecast</p>
+    // </div>
+    // <div class="weatherCard">
+    //     <p>day5</p>
+    //     <p>temp</p>
+    //     <p>forecast</p>
+    // </div>
+    // </article>
