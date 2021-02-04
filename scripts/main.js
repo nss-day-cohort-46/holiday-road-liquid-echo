73ab795d159186f906916cf244d2a853acba2c98
jakefroeb <jake.herman.froeb@gmail.com>
2021-02-04 10:42:22 -0600

    made itinerary function that takes an itinerary obj and converts it to html

--- scripts/main.js
+++ scripts/main.js
@@ -17,6 +17,7 @@ parkSelect().then( () => {
 
 // Test Code Below
 import { Preview } from './preview/Preview.js'
+import { Itinerary } from "./itinerary/Itinerary.js";
 
 let item = {
     id: 123,
@@ -27,3 +28,16 @@ let item = {
 let park = Preview(item)
 
 console.log(park)
+
+let testItinerary = {
+        parkName: "Pretty Park",
+        parkID: 1,
+        eateryName: "Hotdogplace",
+        eateryID: 1,
+        attractionName: "Hall of mirrors",
+        attractionID: 1,
+        date: new Date(),
+        id: 1
+    } 
+    
+console.log(Itinerary(testItinerary))
