5c42093cf3dccea20ebcb3a817968e7a7a9d43b2
Nick Carver <nicholascarver2@outlook.com>
2021-02-04 10:36:26 -0600

    cleaned main

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,10 @@
 import { eaterySelect } from "./eateries/EaterySelect.js";
 import { attractionSelect } from './attractions/AttractionSelect.js';
 import { parkSelect } from './parks/ParkSelect.js'
-import { getAttractionDetail } from "./attractions/AttractionProvider.js";
-import { getParkDetail, getParks } from "./parks/ParkProvider.js";
-import { getEateryDetail } from "./eateries/EateryProvider.js"
 import './preview/PreviewList.js'
 
 parkSelect().then( () => {
     eaterySelect().then( () => {
-        attractionSelect().then( () => {
-            console.log(getParkDetail("77E0D7F0-1942-494A-ACE2-9004D2BDC59E"))
-            console.log(getAttractionDetail("2"))
-            console.log(getEateryDetail("2"))
-        })
+        attractionSelect()
     })
 })
-
-// Test Code Below
-import { Preview } from './preview/Preview.js'
-
-let item = {
-    id: 123,
-    type: "park",
-    name: "Very Pretty Park"
-}
-
-let park = Preview(item)
-
-console.log(park)
