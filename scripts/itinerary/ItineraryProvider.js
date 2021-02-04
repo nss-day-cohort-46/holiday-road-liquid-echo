5b36088c54233270d80d12833fb0d606386fd622
jakefroeb <jake.herman.froeb@gmail.com>
2021-02-04 12:13:54 -0600

    added saveItinerary to Itineraryprovider that uses a fetch post to put data into the db.json also added functions to the preview list that check if all three drobdowns have been selected to activate button, then added a event listener that listens if the button has been pressed and then calls saveItineraries and resets the preview field not only the local variables but also the dom

--- scripts/itinerary/ItineraryProvider.js
+++ scripts/itinerary/ItineraryProvider.js
@@ -1,5 +1,10 @@
 let itinerariesCollection = []
+const eventHub = document.querySelector("#container")
+const dispatchStateChangeEvent = () => {
+    const itinerariesStateChangedEvent = new CustomEvent("itinerariesStateChanged")
 
+    eventHub.dispatchEvent(itinerariesStateChangedEvent)
+}
 export const useItineraries = () => [...itinerariesCollection]
 
 export const getItineraries = () => {
@@ -9,4 +14,17 @@ export const getItineraries = () => {
                 itinerariesCollection = parsedRes
                 console.log(itinerariesCollection)
             })
+}
+export const saveItinerary = itinerary => {
+    debugger
+    return fetch('http://localhost:8088/itineraries', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(itinerary)
+    })
+
+    .then(getItineraries)
+    .then(dispatchStateChangeEvent)
 }
--- scripts/preview/PreviewList.js
+++ scripts/preview/PreviewList.js
@@ -1,5 +1,6 @@
 import { getAttractionDetail } from "../attractions/AttractionProvider.js"
 import { getEateryDetail } from "../eateries/EateryProvider.js"
+import { saveItinerary } from "../itinerary/ItineraryProvider.js"
 import { getParkDetail } from "../parks/ParkProvider.js"
 import { Preview } from "./Preview.js"
 
@@ -8,27 +9,49 @@ const eventHub = document.querySelector("#container")
 let parkHTML = ""
 let attractionHTML = ""
 let eateryHTML = ""
+let eateryObj
+let attractionObj
+let parkObj
 
 
 eventHub.addEventListener("parkSelected", event => {
     if (event.detail.id !==0) {
-        let parkObj = getParkDetail(event.detail.id)
+        parkObj = getParkDetail(event.detail.id)
         parkHTML = Preview(parkObj)
         render()
+        saveButtonActive()
     }
 })
 eventHub.addEventListener("attractionSelected", event => {
     if (event.detail.id !==0) {
-        let attractionObj = getAttractionDetail(event.detail.id)
+        attractionObj = getAttractionDetail(event.detail.id)
         attractionHTML = Preview(attractionObj)
         render()
+        saveButtonActive()
     }
 })
 eventHub.addEventListener("eaterySelected", event => {
     if (event.detail.id !==0) {
-        let eateryObj = getEateryDetail(event.detail.id)
+        eateryObj = getEateryDetail(event.detail.id)
         eateryHTML = Preview(eateryObj)
         render()
+        saveButtonActive()
+    }
+})
+
+eventHub.addEventListener("click", e => {
+    if (e.target.id === "saveItinerary"){
+        const itinerary = {
+                parkName: parkObj.name,
+                parkID: parkObj.id,
+                eateryName: eateryObj.name,
+                eateryID: eateryObj.id,
+                attractionName: attractionObj.name,
+                attractionID: attractionObj.id,
+                date: new Date()
+            }
+            debugger
+        saveItinerary(itinerary).then(clearPreview)
     }
 })
 
@@ -40,6 +63,22 @@ const render = () => {
     <button class="disabled" id="saveItinerary">Save Itinerary</button>
     `
 }
+
+const saveButtonActive = () => {
+    if(parkObj && eateryObj && attractionObj){
+        document.querySelector("#saveItinerary").classList.remove("disabled")
+    }
+}
+
+const clearPreview = () => {
+    parkObj = {}
+    eateryObj = {}
+    attractionObj = {}
+    Preview(parkObj)
+    Preview(eateryObj)
+    Preview(attractionObj)
+}
+
 
     // ${forecastHTML}
     // <article class="preview__forecast">
