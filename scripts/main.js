af4f44beb03c2030b0db45404b7eaccb5b494a2d
Nick Carver <nicholascarver2@outlook.com>
2021-02-05 12:10:44 -0600

    got the directions to provide a response. See comments inside

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ import './preview/PreviewList.js'
 import './preview/PreviewDetail.js'
 import { ItineraryList } from "./itinerary/ItineraryList.js";
 import './itinerary/SaveItinerary.js'
+import { getDirections, getLocation } from "./directions/DirectionProvider.js";
 
 
 parkSelect()
@@ -14,3 +15,11 @@ parkSelect()
 
 
 // TEST CODE
+const nashville = "36.1622296,-86.7743531"
+const park = "37.84054795,-119.8861004"
+
+const nashvilleSearch = "nashville+tn"
+
+getDirections(nashville, park, nashville)
+
+getLocation(nashvilleSearch)
